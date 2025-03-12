bbb0aa5052c4aa39280d83a407f88616842f051d
AgustinPessina00 <apessina@fi.uba.ar>
2025-03-11 23:04:32 -0300

    Convertimos el proyecto a un proyecto de++.
    Co-authored-by: IgnacioLazzaro <ilazzaro@fi.uba.ar>

---
+++ Modules/ConfigGPS/configGPS.h
@@ -0,0 +1,15 @@
+/*
+ * configGPS.h
+ *
+ *  Created on: Mar 11, 2025
+ *      Author: Agustín
+ */
+
+#ifndef MODULES_CONFIGGPS_CONFIGGPS_H_
+#define MODULES_CONFIGGPS_CONFIGGPS_H_
+
+
+
+
+
+#endif /* MODULES_CONFIGGPS_CONFIGGPS_H_ */
