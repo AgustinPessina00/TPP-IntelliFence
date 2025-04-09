7dd64bc5fb79d5e0d2362cefe0ec2cfcc9e5e4cb
nacho <ilazzaro@fi.uba.ar>
2025-04-08 23:19:40 -0300

    Se agregaron los registros para configurar el LSM6DSO (IMU).
    Co-authored-by: AgustinPessina00 <apessina@fi.uba.ar>

--- Modules/ConfigIMU/configIMU.cpp
+++ Modules/ConfigIMU/configIMU.cpp
@@ -12,40 +12,50 @@
 /* Private includes ----------------------------------------------------------*/
 
 
-
-
-void acel_gyro_init(){
-
-	// Inicializar Acelerómetro (Low-power) -> 0x4A
-	LSM6DSO_WriteReg(CTRL1_XL, 0x4A); // 0bxxxx xxxx
-	/*
-	data[0] = CTRL1_XL;
-	data[1] = 0x4A;		// 0100 | 10 | 1 | 0	-> ODR = 52Hz(LP Mode) | FS=±4g | 0
-	HAL_I2C_Master_Transmit(&hi2c1, LSM6DSO_ADDR, data, 2, HAL_MAX_DELAY);
-	*/
-
-	// Inicializar giroscopio (Power-down)
-	data[0] = CTRL2_G;
-	data[1] = 0x00; // 0000 | 00 | 0 | 0	-> ODR=Power-down | FS=±250dps | 0
-	HAL_I2C_Master_Transmit(&hi2c1, LSM6DSO_ADDR, data, 2, HAL_MAX_DELAY);
-
-	// Inicializar Operating mode Acelerómetro (Low-power)
-	data[0] = CTRL6_C;
-	data[1] = ; //	->	| XL_HM_MODE = 1 (High-performance disabled) |
-	HAL_I2C_Master_Transmit(&hi2c1, LSM6DSO_ADDR, data, 2, HAL_MAX_DELAY);
-
-	// Inicializar Operating mode Giroscopio (DISABLED)
-	data[0] = CTRL7_C;
-	data[1] = ; //	1 | 	->	| G_HM_MODE = 1 (High-performance disabled) |
-	HAL_I2C_Master_Transmit(&hi2c1, LSM6DSO_ADDR, data, 2, HAL_MAX_DELAY);
-
-}
-
-void LSM6DSO_WriteReg(uint8_t reg, uint8_t data) {
+void LSM6DSO_write_register(uint8_t reg, uint8_t value) {
 	uint8_t info[2];
 	info[0] = reg;
-	info[1] = data;
-	HAL_I2C_Master_Transmit(&hi2c1, LSM6DSO_ADDR, info, 2, HAL_MAX_DELAY);
+	info[1] = value;
+	HAL_I2C_Master_Transmit(&hi2c2, LSM6DSO_ADDR, info, 2, HAL_MAX_DELAY);
+}
+
+
+void LSM6DSO_init(){
+
+	// Deshabilita Interface I3C
+	LSM6DSO_WriteReg(CTRL9_XL, 0x02); // 0 | 0 | 0 | 0 | 0 | 0 | 1 | 0	-> | I3C_disable = 1 (Deshabilita interface I3C)|
+
+	// Inicializar Acelerómetro (Low-power)
+	LSM6DSO_WriteReg(CTRL1_XL, 0x28); // 0010 | 10 | 0 | 0	-> ODR = 26Hz(LP Mode) | FS = ±4g | 0 (reserved)
+
+	// Inicializar giroscopio (Power-down)
+	LSM6DSO_WriteReg(CTRL2_G, 0x00); // 0000 | 00 | 0 | 0	-> ODR = Power-down | FS = ±250dps | 0 (reserved)
+
+	// Inicializar Operating mode Acelerómetro (Low-power)
+	LSM6DSO_WriteReg(CTRL6_C, 0x10); //  | 1 |	->	| XL_HM_MODE = 1 (High-performance disabled) |
+
+	// Inicializar Operating mode Giroscopio (DISABLED)
+	LSM6DSO_WriteReg(CTRL7_C, 0x80); // 1 | 	-> G_HM_MODE = 1 (High-performance disabled) |
+
+	// Configurar el THS del Wake-Up
+	LSM6DSO_WriteReg(WAKE_UP_THS, 0x02); // 0 | 0 | 0 | 0 | 0 | 0 | 1 | 0	-> | WK_THS[5:0] = 2 (1LSB x 2 = 125mg) |
+
+	// Configurar duración para acivar Wake-Up
+	LSM6DSO_WriteReg(WAKE_UP_DUR, 0x00); // 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0	-> | WAKE_DUR[1:0] = 0 (1LSB = 1ODR_time = 1/26Hz = 38.5ms) | WAKE_THS_W = 0 (1LSB = FS_XL/2^6 = 4g*1000/2^6 = 62.5mg) |
+
+	// Configurar Filtros de la Interrupción
+	LSM6DSO_WriteReg(TAP_CFG0, 0x00); // 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0	-> | SLOPE_FDS = 0 (Aplica filtro SLOPE)|
+
+	// Habilitar Interrupciones
+	LSM6DSO_WriteReg(TAP_CFG2, 0x80); // 1 | 0 | 0 | 0 | 0 | 0 | 0 | 0	-> INT_EN = 1 (Habilita interrupciones básicas)|
+
+	// Rutear INT1 con la función Wake-Up
+	LSM6DSO_WriteReg(MD1_CFG, 0x20); // 0 | 0 | 1 | 0 | 0 | 0 | 0 | 0	-> | INT1_WU = 1 |
+
+	// Configurar Nivel de Activación de INT1
+	LSM6DSO_WriteReg(CTRL3_C, 0x00); // 0 | 0 | 0 | 0 | 0 | 0 | 0 | 0	-> | H_LACTIVE = 0 (INT1 se activa en ALTO) | PP_OD = 0 (Modo Push-Pull)
+
+
 }
 
 
--- Modules/ConfigIMU/configIMU.h
+++ Modules/ConfigIMU/configIMU.h
@@ -9,30 +9,26 @@
 #define MODULES_CONFIGIMU_CONFIGIMU_H_
 
 // static const uint8_t IMU_ADDRESS = 0xD4;	// 0x6A << 1 // IMU 8-bit Address.
-#define IMU_ADDRESS = 0xD4;	// 0x6A << 1 // IMU 8-bit Address.
+#define LSM6DSO_ADDRESS = 0xD4;	// 0x6A << 1 // IMU 8-bit Address.
 
 // Registro de control
 #define CTRL1_XL		0x10	// Control de acelerómetro (10h = 0x10hexadecimal)
 #define CTRL2_G			0x11	// Control de giroscopio (11h)
-#define CTRL5_C			0x14	//
-#define CTRL6_C         0x15	// 0x0001 1000 Operating Mode Acelerómetro (15h)
-#define CTRL7_C			0x15	// 0x0001 1000 Operating Mode Giroscopio (16h)
-#define CTRL8_XL		0x17
-#define WAKE_UP_THS		0x5B
-#define WAKE_UP_DUR		0x5C
-#define TAP_CFG			0x58
-#define MD1_CFG			0x5E
+#define CTRL3_C			0x12	// Configurar Activación de INT1 (12h)
+#define CTRL6_C         0x15	// Operating Mode Acelerómetro (15h)
+#define CTRL7_C			0x16	// Operating Mode Giroscopio (16h)
+#define CTRL9_XL		0x18	// Deshabilitar Interface I3C (18h)
+#define TAP_CFG0		0x56	// Aplicar Filtro HPF o SLOPE (56h)
+#define TAP_CFG2		0x58	// Habilitar Interrupciones Básicas (58h)
+#define WAKE_UP_THS		0x5B	// Configuración de Wake-up (5Bh)
+#define WAKE_UP_DUR		0x5C	// Duración del Wake-Up (5Ch)
+#define MD1_CFG			0x5E	// Rutear Funciones del pin INT1 (5Eh)
 
 
-/*
- * NACHO: VER SI USAMOS FIFO
- * FIFO_CTRL1 (07h), FIFO_CTRL2 (08h), FIFO_STATUS1 (3Ah), FIFO_STATUS2(3Bh), FIFO_CTRL4 (0Ah).
- */
+// Write register into the LSM6DSO via I2C
+void LSM6DSO_write_register(uint8_t reg, uint8_t data);
 
-// Write of register
-void LSM6DSO_write_reg(uint8_t reg, uint8_t data);
-
-// Initialization of Acelerometer and Gyroscope
-void acel_gyro_init();
+// Initialization of LSM6DSO Accelerometer
+void LSM6DSO_init();
 
 #endif /* MODULES_CONFIGIMU_CONFIGIMU_H_ */
