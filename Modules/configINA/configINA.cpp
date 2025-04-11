c7c7c56275f411f8cc56599abfce644aba32e169
nacho <ilazzaro@fi.uba.ar>
2025-04-10 21:07:01 -0300

    Added configINA folder + .h & .cpp
    Added all configurable registers into .h

---
+++ Modules/configINA/configINA.cpp
@@ -0,0 +1,10 @@
+/*
+ * configINA.cpp
+ *
+ *  Created on: Apr 10, 2025
+ *      Author: nacho
+ */
+
+
+
+
---
+++ Modules/configINA/configINA.h
@@ -0,0 +1,26 @@
+/*
+ * configINA.h
+ *
+ *  Created on: Apr 10, 2025
+ *      Author: nacho
+ */
+
+#ifndef MODULES_CONFIGINA_CONFIGINA_H_
+#define MODULES_CONFIGINA_CONFIGINA_H_
+
+
+
+#define CONFIGREG	0x00	// Configuration Register (00h)
+#define VSHUNTREG	0x01	// Shunt Voltage Register (01h)
+#define VBUSREG		0x02	// Bus Voltage Register (02h)
+#define POWERREG	0x03	// Power Register (03h)
+#define IREG		0x04	// Current Register (04h)
+#define CALREG		0x05	// Calibration Register (05h)
+#define MASKREG		0x06	// Mask/Enable Register (06h)
+#define ALTERREG	0x07	// Altert Limit Register (07h)
+#define MANIDREG	0xFE	// Manufacturer ID Register (FEh)
+#define DIEIDREG	0xFF	// Die ID Register (FFh)
+
+
+
+#endif /* MODULES_CONFIGINA_CONFIGINA_H_ */
