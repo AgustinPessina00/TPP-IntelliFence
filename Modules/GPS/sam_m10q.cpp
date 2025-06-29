d31ece86042345c42363a9fb028899b5b4054ee0
AgustinPessina00 <apessina@fi.uba.ar>
2025-06-29 00:15:23 -0300

    gps driver """finished"""

--- Modules/GPS/sam_m10q.cpp
+++ Modules/GPS/sam_m10q.cpp
@@ -5,8 +5,9 @@
 
 /* Private includes ----------------------------------------------------------*/
 
-SamM10q::SamM10q(uint8_t i2cAddr) {
+SamM10q::SamM10q(uint8_t i2cAddr, TinyGPSPlus *trackerGPS) {
 	this->i2cAddr = i2cAddr;
+    this->trackerGPS = trackerGPS;  // Debo declarar previamente:: TinyGPSPlus gps; -> SamM10q gpsModule(0x42, &gps);
 	this->header[0] = UBX_HEADER1;
 	this->header[1] = UBX_HEADER2;
     this->msgClass 	= VALSET_CLASS;
@@ -16,15 +17,61 @@ SamM10q::SamM10q(uint8_t i2cAddr) {
     this->reserved	= RESERVED;
 }
 
-void SamM10q::create_msgGPS() {
+void SamM10q::read_nmea_stream(I2C_HandleTypeDef *hi2c) {
+    uint8_t buffer[NMEA_BUFFER_SIZE];
 
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(hi2c, i2cAddr, 0xFF, I2C_MEMADD_SIZE_8BIT, buffer, NMEA_BUFFER_SIZE, HAL_MAX_DELAY);
+
+    if (status != HAL_OK) {
+        // Podés agregar manejo de error acá si querés
+        return;
+    }
+
+    for (uint8_t i = 0; i < NMEA_BUFFER_SIZE; ++i) {
+        gps->encode(buffer[i]); // Podemos hacer también (*gps).encode(buffer[i])
+    }
+}
+
+void SamM10q::update_location_and_time() {
+    if (trackerGPS->location.isUpdated() && trackerGPS->location.isValid()) {
+        latitude = trackerGPS->location.lat();
+        longitude = trackerGPS->location.lng();
+    }
+
+    if (trackerGPS->date.isUpdated() && trackerGPS->date.isValid()) {
+        uint16_t year = trackerGPS->date.year();   // Ej: 2025
+        uint8_t month = trackerGPS->date.month();  // Ej: 6
+        uint8_t day   = trackerGPS->date.day();    // Ej: 12
+
+        fechaUTC = (year % 100) * 10000 + month * 100 + day; // yymmdd
+    }
+
+    if (trackerGPS->time.isUpdated() && trackerGPS->time.isValid()) {
+        uint8_t hour = trackerGPS->time.hour();
+        uint8_t minute = trackerGPS->time.minute();
+        uint8_t second = trackerGPS->time.second();
+        horaUTC = hour * 10000 + minute * 100 + second; // hhmmss como entero
+    }
 }
 
 void SamM10q::configure_gps() {
+    std::vector<uint8_t> sendMsgRAM, sendMsgBBR;
 
+    for(size_t i = 0; i < m10q_data.size(); i++){       
+    	// GENERO EL MENSAJE.
+    	sendMsgRAM = build_full_message_from_index(i, RAM);
+    	sendMsgBBR = build_full_message_from_index(i, BBR);
+
+    	// ENVÍO EL MENSAJE.
+        // NACHO: ¡¡ RECORDAR EL TIEMPO ENTRE MSG DE SIGNAL Y MSG DE SIGNAL!! Ver Interface Description.
+        //PESSI: AGREGO EL DELAY EN LA FUNCIÓN send_message.
+
+		send_message(&hi2c2, sendMsgRAM, 15);
+		send_message(&hi2c2, sendMsgBBR, 15);
+    }
 }
 
-std::vector<uint8_t> SamM10q::build_full_message_from_index(size_t i) {
+std::vector<uint8_t> SamM10q::build_full_message_from_index(size_t i, uint8_t layer) {
 	// Validación simple
     if (i >= m10q_data.size() || i >= m10q_checksum.size()) {
     	// Índice fuera de rango → devolvemos mensaje vacío
@@ -33,8 +80,6 @@ std::vector<uint8_t> SamM10q::build_full_message_from_index(size_t i) {
 
     const std::vector<uint8_t>& payload   = m10q_data[i];
     const std::vector<uint8_t>& checksum  = m10q_checksum[i];
-
-    uint8_t layer = RAM; // Por defecto usamos RAM. Habría que ver si le pasamos el parámetro a la función.
 
     // Llamamos a la función que arma el frame UBX completo
     return build_ubx_message(layer, payload, checksum);
@@ -71,7 +116,16 @@ vector<uint8_t> SamM10q::build_ubx_message(uint8_t layer, const std::vector<uint
 	message.push_back(checksum[0]); // CK_A
     message.push_back(checksum[1]);	// CK_B
 
-	return vector<uint8_t>();
+	return message;
+}
+
+HAL_StatusTypeDef SamM10q::send_message(I2C_HandleTypeDef* hi2c, const std::vector<uint8_t>& message, uint32_t delay_ms) {
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Write(hi2c,i2cAddr, 0xFF, I2C_MEMADD_SIZE_8BIT, message.data(), message.size(), HAL_MAX_DELAY);
+
+    // Delay para que el módulo procese
+    HAL_Delay(delay_ms);
+
+    return status;
 }
 
 void SamM10q::ubx_calculate_checksum(UBX_Message *msg) {
--- Modules/GPS/sam_m10q.h
+++ Modules/GPS/sam_m10q.h
@@ -2,6 +2,7 @@
 #define MODULES_GPS_SAM_M10Q_H_
 
 #include "sam_m10q_KEYID.h"
+#inclde "TinyGPSPlus/TinyGPS++.h"
 
 #define UBX_HEADER1  0xB5
 #define UBX_HEADER2  0x62
@@ -12,20 +13,31 @@
 #define BBR 0x02
 #define RESERVED 0x0000
 
+#define NMEA_BUFFER_SIZE	64
+
 class SamM10q {
 public:
-	SamM10q(uint8_t i2cAddr);
+	SamM10q(uint8_t i2cAddr, TinyGPSPlus *trackerGPS);
 
+	void read_nmea_stream(I2C_HandleTypeDef *hi2c);
+	void update_location_and_time();
 
 private:
-	void create_msgGPS();
 	void configure_gps();
 	std::vector<uint8_t> build_full_message_from_index(size_t i);
 	std::vector<uint8_t> build_ubx_message(uint8_t layer, const std::vector<uint8_t> payload, const std::vector<uint8_t> checksum);
+	HAL_StatusTypeDef send_message(I2C_HandleTypeDef *hi2c, const std::vector<uint8_t> &message, uitn32_t delay_ms);
 	void ubx_calculate_checksum(UBX_Message *msg)
+
+public:
+	double latitude = 0.0;
+	double longitude = 0.0;
+	uint32_t fechaUTC = 0; //yymmdd
+	uint32_t horaUTC = 0; //hhmmss
 
 private:
     uint8_t i2cAddr;
+	TinyGPSPlus *trackerGPS;
 	uint8_t header[2];
 	uint8_t msgClass;
 	uint8_t msgID;
