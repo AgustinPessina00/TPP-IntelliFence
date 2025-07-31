d433dd300f9626593ac6332d06b5add9f5acb685
AgustinPessina00 <apessina@fi.uba.ar>
2025-07-30 22:26:35 -0300

    Add zone and distance in the same message.

--- CM4/Core/Inc/messages_id.h
+++ CM4/Core/Inc/messages_id.h
@@ -25,13 +25,14 @@
 #define MSG_ID_REQUEST_INA_IMU   0x29
 
 // Cerca virtual
-#define MSG_ID_FENCE_UPDATE                 0x30  // Nueva cerca virtual enviada
-#define MSG_ID_FENCE_STATUS                 0x31  // Estado de la cerca actual
-#define MSG_ID_FENCE_BREACH                 0x32  // Animal se salió del perímetro
-#define MSG_ID_REQUEST_DISTANCE_TO_FENCE    0x33  // Request Distancia actual al límite
-#define MSG_ID_REQUEST_ZONE_TO_FENCE        0x34  // Request Zona actual
-#define MSG_ID_SEND_DISTANCE_TO_FENCE       0x35  // Distancia actual al límite
-#define MSG_ID_SEND_ZONE_TO_FENCE           0x36  // Zona actual
+#define MSG_ID_FENCE_UPDATE                     0x30    // Nueva cerca virtual enviada
+#define MSG_ID_FENCE_STATUS                     0x31    // Estado de la cerca actual
+#define MSG_ID_FENCE_BREACH                     0x32    // Animal se salió del perímetro
+#define MSG_ID_REQUEST_DISTANCE_TO_FENCE        0x33    // Request Distancia actual al límite
+#define MSG_ID_REQUEST_ZONE_TO_FENCE            0x34    // Request Zona actual
+#define MSG_ID_SEND_DISTANCE_TO_FENCE           0x35    // Distancia actual al límite
+#define MSG_ID_SEND_ZONE_TO_FENCE               0x36    // Zona actual
+#define MSG_ID_SEND_ZONE_AND_DISTANCE_TO_FENCE  0x37    // Zona actual y Distancia actual al límite
 
 // Estímulos individuales
 #define MSG_ID_STIMULUS_VIBRATION_REQUEST   0x40  // Pedido de vibración
--- CM4/Core/Src/threads/distanceTask.cpp
+++ CM4/Core/Src/threads/distanceTask.cpp
@@ -30,8 +30,18 @@ void distanceToLimitTask(void *argument) {
                 msgToSend = new Message(MSG_ID_SEND_DISTANCE_TO_FENCE, ModuleId_t::DISTANCE, ModuleId_t::FSM, sizeof(float));
                 std::memcpy(msgToSend->payload, &minDistance, sizeof(float));
                 break;
+            case MSG_ID_SEND_ZONE_AND_DISTANCE_TO_FENCE:
+                msgToSend = new Message(MSG_ID_SEND_ZONE_AND_DISTANCE_TO_FENCE, ModuleId_t::DISTANCE, ModuleId_t::FSM, 
+                sizeof(uint8_t) + sizeof(float)); // 5 bytes
+                std::memcpy(msgToSend->payload, &zoneCode, sizeof(uint8_t));
+                std::memcpy(msgToSend->payload + sizeof(uint8_t), &minDistance, sizeof(float));
+                osMessageQueuePut(dispatcherQueueHandle, msgToSend, 0, 0);
+                break;
+
             default:
                 break;
+            }
+            
             }
             if(msgToSend)
                 osMessageQueuePut(dispatcherQueueHandle, msgToSend, 0, 0);
