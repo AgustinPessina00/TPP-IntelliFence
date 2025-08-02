4052b0933a54440c243d08e7cff054885145924e
Ignacio Lazzaro <ilazzaro@fi.uba.ar>
2025-08-02 13:39:05 -0300

    Added zone.h to share zone_t between fence.h and cow.h

--- CM4/Core/Inc/fence.h
+++ CM4/Core/Inc/fence.h
@@ -3,24 +3,11 @@
 #define FENCE_H
 
 #include <vector>
+#include "zone.h"
 
 #define TOTAL_TRESHOLDS  5
 
 typedef float threshold_t;
-
-typedef enum {
-    GREEN_ZONE       = 0x00,  //  Dentro del cerco virtual (sin estímulo)
-    
-    LIGHT_BLUE_ZONE  = 0x01,  //  Buzzer leve (frecuencia baja, duty bajo)
-    BLUE_ZONE        = 0x02,  //  Buzzer medio
-    DARK_BLUE_ZONE   = 0x03,  //  Buzzer intenso
-    
-    YELLOW_ZONE      = 0x04,  //  Buzzer + vibración
-    
-    RED_ZONE         = 0x05,  //  Vibración intensa (sin shock)
-    BLACK_ZONE       = 0x06   //  Se escapó → alerta crítica
-} zone_t;
-
 
 struct Vertex {
     double latitude;
