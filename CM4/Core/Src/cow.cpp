e4915ceec1a23a25b9c04ebe8e6139f3a94cd9a0
Ignacio Lazzaro <ilazzaro@fi.uba.ar>
2025-08-07 21:29:28 -0300

    Completed the Cow constructor

--- CM4/Core/Src/cow.cpp
+++ CM4/Core/Src/cow.cpp
@@ -4,6 +4,15 @@
 // TODO: Falta declarar en el constructor los otros parámetros de cow.
 Cow::Cow(uint16_t id) : id(id), state(CowState::SLEEP) {}
 
+Cow::Cow(uint16_t id) {
+    this->id = id;
+    this->position = {0, 0};
+    this->acceleration = {0, 0, 0};
+    this->state = CowState::SLEEP;
+    this->currentZone = zone_t::BLACK_ZONE;
+    distanceToLimit = -1;
+}
+
 void Cow::updatePosition(Position pos) {
     position = pos;
 }
